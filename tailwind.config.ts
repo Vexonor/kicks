3d7a2664971f0515af06b7d4bbe6fa4e22c132be
Vexonor <vexonordark@gmail.com>
2024-11-02 22:42:42 +0700

    first commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,15 +5,25 @@ const config: Config = {
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    './node_modules/preline/preline.js'
   ],
   theme: {
     extend: {
       colors: {
+        "Blue" : "#4968E1",
+        "Dark-Gray" : "#222220",
+        "FA-White" : "#F9F9F9",
+        "Gray" : "#E6E6E2",
+        "Gray-Main" : "#6F6F6D",
+        "Yellow" : "#FEA42E",
+        "Quick-Silver" : "#A5A5A5",
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
     },
   },
-  plugins: [],
+  plugins: [
+      require('preline/plugin')
+  ],
 };
 export default config;
